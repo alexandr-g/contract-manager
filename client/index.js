852df6e2a6c9f2d7a2d948c323991fe29785939b
Alexander Grischuk <alexgrischuk@gmail.com>
2017-07-30 22:49:09 +0200

    Add initial client side code

---
+++ client/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import App from './components/App'
+import Categories from './components/Categories'
+import CategoryCreate from './components/CategoryCreate'
+
+const client = new ApolloClient({})
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Categories} />
+          <Route path="category/new" component={CategoryCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'))
