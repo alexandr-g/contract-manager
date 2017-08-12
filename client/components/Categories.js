9126ebddb950e78b7b8f0453f8cb9be8e48e049f
Alexander Grischuk <alexgrischuk@gmail.com>
2017-08-12 21:33:21 +0200

    Refactor <Categories />

--- client/components/Categories.js
+++ client/components/Categories.js
@@ -2,49 +2,45 @@ import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 import { Link } from 'react-router'
+import { compose, withState, withHandlers } from 'recompose'
 import query from '../queries/fetchCategories'
 import mutation from '../mutations/deleteCategory'
 
-class Categories extends Component {
-  onCategoryDelete(id) {
-    this.props
-      .mutate({ variables: { id } })
-      .then(() => this.props.data.refetch())
+const Categories = ({ onCategoryDelete, data: { loading, categories } }) => {
+  if (loading) {
+    return <div>loading...</div>
   }
 
-  renderCategories() {
-    return this.props.data.categories.map(({ id, name }) => {
-      return (
-        <li key={id} className="collection-item">
-          {name}
-          <i
-            className="material-icons"
-            onClick={() => this.onCategoryDelete(id)}
-          >
-            delete
-          </i>
-        </li>
-      )
-    })
-  }
-
-  render() {
-    if (this.props.data.loading) {
-      return <div>loading...</div>
-    }
-
-    return (
-      <div>
-        <h2>Categories</h2>
-        <ul className="collection">
-          {this.renderCategories()}
-        </ul>
-        <Link to="categories/new" className="btn-floating btn-large red right">
-          <i className="material-icons">add</i>
-        </Link>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul className="collection">
+        <div>
+          {categories.map(({ id, name }) =>
+            <li key={id} className="collection-item">
+              {name}
+              <i
+                className="material-icons"
+                onClick={() => onCategoryDelete(id)}
+              >
+                delete
+              </i>
+            </li>
+          )}
+        </div>
+      </ul>
+      <Link to="categories/new" className="btn-floating btn-large red right">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+  )
 }
 
-export default graphql(mutation)(graphql(query)(Categories))
+export default compose(
+  graphql(mutation),
+  graphql(query),
+  withHandlers({
+    onCategoryDelete: ({ mutate, data }) => id =>
+      mutate({ variables: { id } }).then(() => data.refetch()),
+  })
+)(Categories)
