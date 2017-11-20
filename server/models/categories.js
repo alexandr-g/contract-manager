4d0d200c180f59309c8e78e51445418485808045
Alexander Grischuk <alexgrischuk@gmail.com>
2017-11-20 20:01:41 +0100

    Clean up category schema and model

--- server/models/categories.js
+++ server/models/categories.js
@@ -3,32 +3,6 @@ const Schema = mongoose.Schema
 
 const CategorySchema = new Schema({
   name: { type: String },
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'user',
-  // },
-  // lyrics: [
-  //   {
-  //     type: Schema.Types.ObjectId,
-  //     ref: 'lyric',
-  //   },
-  // ],
 })
-
-// CategorySchema.statics.addLyric = function(id, content) {
-//   const Lyric = mongoose.model('lyric')
-
-//   return this.findById(id).then(song => {
-//     const lyric = new Lyric({ content, song })
-//     song.lyrics.push(lyric)
-//     return Promise.all([lyric.save(), song.save()]).then(
-//       ([lyric, song]) => song
-//     )
-//   })
-// }
-
-// CategorySchema.statics.findLyrics = function(id) {
-//   return this.findById(id).populate('lyrics').then(song => song.lyrics)
-// }
 
 mongoose.model('categories', CategorySchema)
