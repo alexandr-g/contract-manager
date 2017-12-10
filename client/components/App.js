8a017f22bb995cf5561465fbf35e9a28e630070a
Alexander Grischuk <alexgrischuk@gmail.com>
2017-12-10 18:55:57 +0100

    Add Simple App bar

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 
 export default ({ children }) => {
   return (
-    <div className="container">
-      {children}
+    <div className={{ width: '100%' }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            Contract Manager
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="container">
+        {children}
+      </div>
     </div>
   )
 }
